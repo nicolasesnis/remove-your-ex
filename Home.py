3ee504c1156d0556dfb2e77c6def014fe38d8da9
Nicolas Esnis <nicolas.esnis@gmail.com>
2022-11-18 17:51:46 -0700

    first commit

---
+++ Home.py
@@ -0,0 +1,5 @@
+import streamlit as st 
+
+st.title('Remove Your Ex')
+st.subheader("Quick and easy tool to remove your ex from any picture")
+st.write('All rights reserved - Send dollars and nudes to edmond@smallpepe.com')
